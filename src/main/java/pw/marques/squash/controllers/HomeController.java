05848916e9544d009a86928ee90386f81606dbf0
Raphael Marques <raphi.rm@gmail.com>
2014-03-09 11:18:25 +0100

    change from hsql to mysql for persistent storage.

--- src/main/java/pw/marques/squash/controllers/HomeController.java
+++ src/main/java/pw/marques/squash/controllers/HomeController.java
@@ -2,6 +2,8 @@ package pw.marques.squash.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import pw.marques.squash.domain.Address;
@@ -26,5 +28,17 @@ public class HomeController extends AbstractController {
 		Address address = new Address();
 		address.setStreet1("BLABLA");
 		return new ModelAndView("api/api", "api", address);
+	}
+	@RequestMapping(value = "/register.html", method = RequestMethod.GET)
+	public String registerPage(){
+		log.info("/register.html ");
+		return "home/register";
+		
+	}
+	@RequestMapping(value = "/register.html", method = RequestMethod.POST)
+	public String register(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password, @RequestParam(value="Street", defaultValue="No Street") String street){
+		log.info("/register.html ");
+		return "redirect:index.html";
+		 
 	}
 }
